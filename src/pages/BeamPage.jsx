38834684aeae3c57822dbb1b59ec8ee2f20ea09e
mercuri-biswajit <bisu.rnj@gmail.com>
2026-03-13 13:42:26 +0530

    asdasda
    Signed-off-by: mercuri-biswajit <bisu.rnj@gmail.com>

--- src/pages/BeamPage.jsx
+++ src/pages/BeamPage.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { C, F, badge } from "@styles/tokens";
-import './BeamPage.css';
 import {
   Card,
   TwoCol,
